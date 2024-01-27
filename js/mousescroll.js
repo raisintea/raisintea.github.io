e9b3ee208238c1610b9c9c7a5e584ab8e3cc8874
raisintea <62986418+raisintea@users.noreply.github.com>
2024-01-27 21:06:15 +0900

    fix scroll indicator
    disappears on end of page instead of on scroll

--- js/mousescroll.js
+++ js/mousescroll.js
@@ -1,22 +1,8 @@
-// window.onscroll = function() {
-//     // Set the height to check for
-//   var appear = 20
-//   if (window.pageYOffset >= appear) {
-//     // If more show the element
-//     document.getElementById("mouse-scroll").style.opacity = '1'
-//     document.getElementById("mouse-scroll").style.pointerEvents = 'all'
-//   } else {
-//     // Else hide it
-//     document.getElementById("mouse-scroll").style.opacity = '0'
-//     document.getElementById("mouse-scroll").style.pointerEvents = 'none'
-//   }
-// }
-
 $(document).ready(function() {
   
   $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if (scroll >= 1) {
+    var scroll = $(window).scrollTop() + $(window).height();
+    if (scroll == $(document).height()) {
       $('#mouse-scroll').addClass('fade');
     } else{
       $('#mouse-scroll').removeClass('fade');
