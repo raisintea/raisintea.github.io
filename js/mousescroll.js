72de5913a065c1571893e2e2aa2c1529f8f00602
raisintea <62986418+raisintea@users.noreply.github.com>
2024-01-27 23:58:36 +0900

    fix scroll

--- js/mousescroll.js
+++ js/mousescroll.js
@@ -9,3 +9,33 @@ $(document).ready(function() {
     }
   })
 });
+
+$.scrollify({
+    section : ".panel",
+    scrollSpeed:1100,
+    scrollbars:false,
+    before:function(i,panels) {
+      var ref = panels[i].attr("data-section-name");
+
+      $(".navbar-nav .active").removeClass("active");
+
+      $(".navbar-nav").find("a[href=\"#" + ref + "\"]").addClass("active");
+    },
+    afterRender:function() {
+      var navbar = "<ul class=\"navbar-nav\">";
+      var activeClass = "";
+      $(".panel").each(function(i) {
+        activeClass = "";
+        if(i===$.scrollify.currentIndex()) {
+          activeClass = "active";
+        }
+        navbar += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+      });
+
+      navbar += "</ul>";
+
+      $(".home").append(navbar);
+
+      $(".navbar a").on("click",$.scrollify.move);
+    }
+  });
