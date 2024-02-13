af8996e3f3aca10c8757a8e4aba10573310211b8
raisintea <62986418+raisintea@users.noreply.github.com>
2024-02-14 01:09:52 +0900

    disable scrollify

--- js/mousescroll.js
+++ js/mousescroll.js
@@ -1,50 +1,50 @@
-// $(document).ready(function() {
+$(document).ready(function() {
   
-//   $(window).scroll(function() {
-//     var scroll = $(window).scrollTop() + $(window).height();
-//     if (scroll == $(document).height()) {
-//       $('#mouse-scroll').addClass('fade');
-//     } else{
-//       $('#mouse-scroll').removeClass('fade');
-//     }
-//   })
-// });
-
-$.scrollify({
-    section:".panel",
-    scrollSpeed:1100,
-    scrollbars:false,
-    overflowscroll:true,
-    interstitialSection:".header",
-    before:function(i,panels) {
-      var ref = panels[i].attr("data-section-name");
-
-      $(".navbar-nav .active").removeClass("active");
-
-      $(".navbar-nav").find("a[href=\"#" + ref + "\"]").addClass("active");
-
-      if(ref == "others"){
-        $('#mouse-scroll').addClass('fade');
-      }
-      else{
-        $('#mouse-scroll').removeClass('fade');
-      }
-    },
-    afterRender:function() {
-      var navbar = "<ul class=\"navbar-nav\">";
-      var activeClass = "";
-      $(".panel").each(function(i) {
-        activeClass = "";
-        if(i===$.scrollify.currentIndex()) {
-          activeClass = "active";
-        }
-        navbar += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
-      });
-
-      navbar += "</ul>";
-
-      $(".home").append(navbar);
-
-      $(".navbar a").on("click",$.scrollify.move);
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop() + $(window).height();
+    if (scroll == $(document).height()) {
+      $('#mouse-scroll').addClass('fade');
+    } else{
+      $('#mouse-scroll').removeClass('fade');
     }
-  });
+  })
+});
+
+// $.scrollify({
+//     section:".panel",
+//     interstitialSection:".panel-content",
+//     scrollSpeed:1100,
+//     scrollbars:false,
+//     overflowscroll:true,
+//     before:function(i,panels) {
+//       var ref = panels[i].attr("data-section-name");
+
+//       $(".navbar-nav .active").removeClass("active");
+
+//       $(".navbar-nav").find("a[href=\"#" + ref + "\"]").addClass("active");
+
+//       if(ref == "others"){
+//         $('#mouse-scroll').addClass('fade');
+//       }
+//       else{
+//         $('#mouse-scroll').removeClass('fade');
+//       }
+//     },
+//     afterRender:function() {
+//       var navbar = "<ul class=\"navbar-nav\">";
+//       var activeClass = "";
+//       $(".panel").each(function(i) {
+//         activeClass = "";
+//         if(i===$.scrollify.currentIndex()) {
+//           activeClass = "active";
+//         }
+//         navbar += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+//       });
+
+//       navbar += "</ul>";
+
+//       $(".home").append(navbar);
+
+//       $(".navbar a").on("click",$.scrollify.move);
+//     }
+//   });
